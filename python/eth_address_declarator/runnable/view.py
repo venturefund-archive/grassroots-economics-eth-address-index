4311c4ee1d5c7cb8ecf94ca95b5e5b344a97f7b6
nolash <dev@holbrook.no>
2021-01-05 14:22:34 +0100

    Add declarator viewer cli

--- python/eth_address_declarator/runnable/view.py
+++ python/eth_address_declarator/runnable/view.py
@@ -1,4 +1,4 @@
-"""Adds a new Address declaration
+"""View address declaration, and attempt to resolve contents
 
 .. moduleauthor:: Louis Holbrook <dev@holbrook.no>
 .. pgp:: 0826EDA1702D1E87C6E2875121D2E7BB88C2A746 
@@ -6,6 +6,7 @@
 """
 
 # standard imports
+import urllib
 import os
 import json
 import argparse
@@ -28,14 +29,24 @@ argparser = argparse.ArgumentParser()
 argparser.add_argument('-p', '--provider', dest='p', default='http://localhost:8545', type=str, help='Web3 provider url (http only)')
 argparser.add_argument('-r', '--contract-address', dest='r', type=str, help='Address declaration contract address')
 argparser.add_argument('-o', '--declarator-address', dest='o', type=str, help='Signing address for the declaration')
+argparser.add_argument('--resolve', action='store_true', help='Attempt to resolve the hashes to actual content')
+argparser.add_argument('--resolve-http', dest='resolve_http', type=str, help='Base url to look up content hashes')
 argparser.add_argument('--abi-dir', dest='abi_dir', type=str, default=data_dir, help='Directory containing bytecode and abi (default: {})'.format(data_dir))
 argparser.add_argument('-v', action='store_true', help='Be verbose')
-argparser.add_argument('address', type=str, help='Ethereum address to add declaration to')
-argparser.add_argument('declaration', type=str, help='SHA256 sum of endorsement data to add')
+argparser.add_argument('address', type=str, help='Ethereum declaration address to look up')
 args = argparser.parse_args()
 
 if args.v:
     logg.setLevel(logging.DEBUG)
+
+
+def try_sha256(s):
+    r = urllib.request.urlopen(os.path.join(args.resolve_http, s.hex()))
+    return r.read().decode('utf-8')
+
+def try_utf8(s):
+    return s.decode('utf-8')
+
 
 def main():
     w3 = web3.Web3(web3.Web3.HTTPProvider(args.p))
@@ -51,8 +62,23 @@ def main():
 
     c = w3.eth.contract(abi=abi, address=args.r)
 
-    tx_hash = c.functions.addDeclaration(args.address, args.declaration).transact()
-    print(tx_hash.hex())
+    declarations = c.functions.declaration(w3.eth.defaultAccount, args.address).call()
+
+    for d in declarations:
+        if not args.resolve:
+            print(d.hex())
+            continue
+        if args.resolve_http:
+            try:
+                r = try_sha256(d)
+                print(r)
+                continue
+            except urllib.error.HTTPError:
+                pass
+        try:
+            print(try_utf8(d))
+        except UnicodeDecodeError:
+            pass
 
 
 if __name__ == '__main__':
